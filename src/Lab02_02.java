9cc8d26e52d80f2713c050096e5c6f4aef524174
Darius <vaidagabrieldarius@yahoo.com>
2022-10-21 10:48:57 +0300

    Lab02-6.2

---
+++ src/Lab02_02.java
@@ -0,0 +1,8 @@
+public class Lab02_02 {
+    public static void main(String[] args) {
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println((int)Math.floor(Math.random() * 49+1));
+        }
+    }
+}
